25c05399c83ee29eb1ec6ee39089063e3e745709
agramian <abtin.gramian@gmail.com>
2016-06-04 00:44:42 -0700

    corrected copy command to copy entire directory

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ var generators = require('yeoman-generator');
 module.exports = generators.Base.extend({
   copyTemplate: function () {
      this.fs.copy(
-       this.templatePath('*'),
+       this.templatePath('**'),
        this.destinationRoot()
      )    
   }
