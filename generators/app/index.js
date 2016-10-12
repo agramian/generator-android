1a5117cc57db3f0d1892814e54d63d7c43719747
agramian <abtin.gramian@gmail.com>
2016-10-11 22:50:23 -0700

    first pass prompting test

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,15 +1,30 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-  copyTemplate: function () {
-     this.fs.copy(
-       this.templatePath('**'),
-       this.destinationRoot()
-     )
-     // rename gitignore to .gitignore
-     this.fs.move(
-       this.destinationPath('gitignore'),
-       this.destinationPath('.gitignore')
-     )
-  }
+  prompting: function () {
+      return this.prompt([{
+        type    : 'input',
+        name    : 'org',
+        message : 'Organization name for package com.[org].app',
+        default : this.appname
+      }, {
+        type    : 'input',
+        name    : 'appname',
+        message : 'Application name for package com.org.[app]'
+      }]).then(function (answers) {
+        this.log('org', answers.org);
+        this.log('app', answers.appname);
+        /*
+        this.fs.copy(
+          this.templatePath('**'),
+          this.destinationRoot()
+        )
+        // rename gitignore to .gitignore
+        this.fs.move(
+          this.destinationPath('gitignore'),
+          this.destinationPath('.gitignore')
+        )
+        */
+      }.bind(this));
+    }
 });
